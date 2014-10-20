fa6e35c30a340b2be5b254e93c1eb31b245cca16
forflo <forflo@users.noreply.github.com>
2014-10-20 14:00:36 +0200

    Update configure.sh

--- vim/configure.sh
+++ vim/configure.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+shellyEditorVim(){
+  clog 2 "[shellyEditorVim()]" Setting link for .vim
+  ln -s $PWD/vim/dotVim ~/.vim || {
+    clog 1 "[shellyEditorVim()]" Link could not be set
+    return 1
+  }
+  
+  return 0
+}
+
+shellyEditorVim
