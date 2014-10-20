6709be053b3f064714f94c794591a8c17dcf68b0
forflo <forflo@users.noreply.github.com>
2014-10-20 21:59:28 +0200

    Update configure.sh

--- vim/configure.sh
+++ vim/configure.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-shellyEditorVim(){
-  clog 2 "[shellyEditorVim()]" Setting link for .vim
+shellyEditor_vim(){
+  clog 2 "[shellyEditor_vim()]" Setting link for .vim
+  if [ -e ~/.vim -a \( ! -L ~/.vim \) ]; then
+    clog 1 "[shellyEditor_vim()]" ~/.vim exists but is no symbolic link. Won\'t remove!
+    return 1
+  elif [ -e ~/.vim -a -L ~/.vim ]; then
+    rm ~/.vim || {
+      clog 1 "[shellyEditor_vim()]" Could not remove ~/.vim!
+      return 1
+    }
+  else
+    # just for code cuteness. This case does not require
+    # any action!
+  fi
+  
   ln -s $PWD/vim/dotVim ~/.vim || {
-    clog 1 "[shellyEditorVim()]" Link could not be set
+    clog 1 "[shellyEditor_vim()]" Link could not be set
     return 1
   }
   
   return 0
 }
 
-shellyEditorVim
+shellyEditor_vim
