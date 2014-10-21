d1aced8a1a3c5ac2f9fa058f9f98a98d06650ba2
forflo <forflo@users.noreply.github.com>
2014-10-21 21:02:14 +0200

    Update shellyEditorMain.sh

--- shellyEditorMain.sh
+++ shellyEditorMain.sh
@@ -8,10 +8,21 @@ SHELLYE_subdirs=(
   "emacs"
 )
 
-shellyEditorMain(){
+shellyEditor_init(){
+  type clog > /dev/null || {
+    echo "[shellyEditor_init()]" Could not find logging function. Will define one!
+    function clog(){
+      shift; echo "$@"; return 0
+    }
+  }
+
+  return 0
+}
+
+shellyEditor_start(){
   for i in ${SHELLYE_subdirs[*]}; do
     . $i/configure.sh || {
-      clog 1 "[shellyEditor_init()]" Sourcing of $i/init.sh failed!
+      clog 1 "[shellyEditor_start()]" Sourcing of $i/init.sh failed!
       return 1
     }
   done
@@ -19,4 +30,16 @@ shellyEditorMain(){
   return 0
 }
 
-shellyEditorMain
+shellyEditor_main(){
+  shellyEditor_init || {
+    clog 1 "[shellyEditor_main()]" Could not initialize vim routines
+    return 1
+  }
+  
+  shellyEditor_start || {
+    clog 1 "[shellyEditor_main()]" Could not start editor routines
+    return 1
+  }
+}
+
+shellyEditor_main
